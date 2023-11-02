f314c6141a2f6d6d8b51ce8302ab8201a4b42c05
gsmith <georgesmith812@gmail.com>
2023-11-02 08:06:12 -0400

    Added user input slider to control grid size and clear current grid function
    Added user input slider to control size of the grid using event listeners and created a helper function to clear the grid and rerender the new grid size

--- index.js
+++ index.js
@@ -1,25 +1,66 @@
 // Grab container div for grid and append initial default grid of 16x16 squares
 
 const gridContainer = document.querySelector('.gridContainer');
+const gridSlider = document.querySelector('.gridSlider');
+const sizeLabel = document.querySelector('label');
 
-//set default value of grid size to 16x16
 
-let gridHeight = 16;
-let gridWidth = 16;
+//set default value of grid size to 16x16 and label text to match
+gridSlider.value = 16;
+sizeLabel.innerText = `${gridSlider.value} x ${gridSlider.value}`
+
+// Create a handler function to handle changes to gridSlider value
+
+
+
+gridSlider.addEventListener('change', e => {
+    gridSlider.value = e.target.value;
+    clearGrid();
+    createGrid(gridSlider.value);
+})
+
+gridSlider.addEventListener('mousemove', e => {
+    gridSlider.value = e.target.value;
+    sizeLabel.innerText = `${gridSlider.value} x ${gridSlider.value}`
+})
 
 // create a function to create the intial 16x16 grid
 
-function createGrid(gridHeight, gridWidth) {
-    gridSize = gridHeight * gridWidth;
+function createGrid(gridSliderVal) {
+    gridSize = gridSliderVal ** 2;
     
     // create loop to append a new gridItem into the container for the size of the grid
     for(let i = 0; i < gridSize; i++){
         const gridItem = document.createElement('div');
+    
+        let flexBasisPct = (1 / gridSliderVal) * 100;
+
         gridItem.classList.add('gridItem');
-        let flexBasisPct = (1 / gridWidth) * 100;
         gridItem.setAttribute('style', `flex: 1 1 ${flexBasisPct}%`);
         gridContainer.append(gridItem);
     }
+
+    const squares = document.querySelectorAll('.gridItem');
+
+    // add event listener to each individual square in the grid
+
+    squares.forEach(square => {
+        square.addEventListener('mouseover', e => {
+            e.target.style.backgroundColor = 'black';
+        })
+    });
 }
 
-createGrid(gridHeight, gridWidth);
+// create function to clear grid when slider value changes
+
+function clearGrid(){
+    while(gridContainer.firstChild) {
+        gridContainer.firstChild.remove();
+    }
+}
+
+// Create default grid
+
+createGrid(gridSlider.value);
+
+
